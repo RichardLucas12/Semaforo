25a031e8ed4ec5c9e8adb4343d78205e5ce7049f
richard <richard_lucas.om12@gmail.com>
2020-11-12 16:42:09 -0300

    semaforo

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Nova_pasta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string amarelo = "amarelo";
+            string vermelho = "vermelho";
+             
+           Console.WriteLine("cor qual a cor do semaforo");
+           string cor = Console.ReadLine();
+
+            if (cor == vermelho )
+            {
+                Console.WriteLine("PARE");
+            }
+            else
+            {
+                if(cor == amarelo )
+                {
+                    Console.WriteLine(" sinal, Amarelo atenção diminua a velocidade");
+                }
+                else{
+                    Console.WriteLine("sinal verde, continue em frente");
+                }
+
+            }
+
+        }
+    }
+}
